d216a6a19f4f1d8acfb9ff6cede70ce35e4a7766
ionut.pasat <ipasat10@gmail.com>
2022-08-09 12:03:07 +0300

    added noDuplicates and sorting method
    Signed-off-by: ionut.pasat <ipasat10@gmail.com>

--- Java8.1/src/main/java/StreamExamples.java
+++ Java8.1/src/main/java/StreamExamples.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamExamples {
@@ -14,29 +12,36 @@ public class StreamExamples {
         users.add(user1);
         users.add(user2);
         users.add(user3);
+        users.add(user1);
         Stream<User> myStream = users.stream();
         System.out.println(users);
 
         //2
-        Stream<User> anotherStream = Stream.generate(()->User.createUser()).limit(3);
+        Stream<User> anotherStream = Stream.generate(User::createUser).limit(3);
 
-        myStream.filter(val -> val.getAge() > 50).forEach(val -> System.out.println(val));
+        myStream.filter(val -> val.getAge() > 50).forEach(System.out::println);
 
 
         users.stream().map(val -> {val.setAge(val.getAge() * 2);
             return val;
         })
-                .forEach((val -> System.out.println(val)));
+                .forEach((System.out::println));
 
         Optional<User> last = users.stream().reduce((a, b) -> b);
-        last.ifPresent( (value) -> System.out.println(value));
+        last.ifPresent(System.out::println);
 
         Comparator<User> comparator = (a, b) -> a.getAge() - b.getAge();
 
         Optional<User> youngest = users.stream().min(comparator);
-        youngest.ifPresent((value) -> System.out.println(value));
+        youngest.ifPresent(System.out::println);
 
         Optional<User> oldest = users.stream().max(comparator);
-        oldest.ifPresent((value) -> System.out.println(value));
+        oldest.ifPresent(System.out::println);
+
+        List<User> noDupes = users.stream().distinct().collect(Collectors.toList());
+        System.out.println(noDupes);
+
+        users.stream().filter(val -> val.getAge() > 30).map(val -> {val.setName(val.getName().toUpperCase());
+        return val;}).sorted(comparator.reversed()).forEach(System.out::println);
     }
 }
--- Java8.1/src/main/java/StreamMain.java
+++ Java8.1/src/main/java/StreamMain.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 public class StreamMain {
     public static void main(String[] args) {
         Stream<Integer> stream = Stream.of(1,2,3,4,5,6,7,8,9,10);
-        stream.filter(val -> val > 8).forEach(val -> System.out.println(val));
+        stream.filter(val -> val > 8).forEach(System.out::println);
 
         stream = Stream.of(1,2,3,4,5,6,7,8,9,10);
 
